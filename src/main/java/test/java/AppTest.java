011317f5495e8c8f6f2401eb48ab7c52fcc0312c
munteancodrin <codrinm91@gmail.com>
2024-03-31 20:13:24 +0300

    test

---
+++ src/main/java/test/java/AppTest.java
@@ -0,0 +1,16 @@
+package test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
